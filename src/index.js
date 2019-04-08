fd7314410e3ed12f0ed972e2ec194a343accbb9b
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2019-04-08 19:32:54 +0200

    router build fix

--- src/index.js
+++ src/index.js
@@ -4,23 +4,25 @@
 
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Rent from "./pages/Rent";
 import Contacts from "./pages/Contacts";
 import Error from "./components/Error";
+import Navigation from "./components/Navigation";
 
 const App = () => (
-  <BrowserRouter>
+  <Router>
+    <Navigation />
     <Switch>
-      <Route path="/" component={Home} exact />
+      <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/rent" component={Rent} />
       <Route path="/contacts" component={Contacts} />
       <Route component={Error} />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 render(<App />, document.getElementById("app"));
