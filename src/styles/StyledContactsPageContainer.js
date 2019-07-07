6eae1c7dba6605c31f75d57ba66c2d82d78b09bd
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2019-07-07 11:43:33 +0200

    contact page position absolute added

--- src/styles/StyledContactsPageContainer.js
+++ src/styles/StyledContactsPageContainer.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const StyledContactsPageContainer = styled.div`
+  position: absolute;
+  width: 100%;
   background: linear-gradient(to bottom, #ffffff, #e4f7f9);
   text-align: center;
+  padding: 90px auto;
 `;
